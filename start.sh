66a2fbf47f2435c867ba953e9ffc8af63ade12c6
kalentivan <kalentivan@yandex.ru>
2025-05-25 16:19:19 +0300

    авторизация, тестирование

--- start.sh
+++ start.sh
@@ -5,14 +5,14 @@
 source ./venv/bin/activate
 
 # Ждем, пока Postgres не станет доступен
-until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER; do
+until pg_isready -h $TG_DB_HOST -p $TG_DB_PORT -U $TG_DB_USER; do
   echo "Waiting for postgres..."
   sleep 2
 done
 
 # Создаем базу, если ее нет (предполагается, что есть пользователь с нужными правами)
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -tc "SELECT 1 FROM pg_database WHERE datname = 'tg'" | grep -q 1 || \
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "CREATE DATABASE tg"
+psql -h $TG_DB_HOST -p $TG_DB_PORT -U $TG_DB_USER -tc "SELECT 1 FROM pg_database WHERE datname = 'tg'" | grep -q 1 || \
+psql -h $TG_DB_HOST -p $TG_DB_PORT -U $TG_DB_USER -c "CREATE DATABASE tg"
 
 # Применяем миграции alembic
 alembic upgrade head
