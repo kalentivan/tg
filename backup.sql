26fcdbd8970527789cbad8dc28dfc785e1b72d8f
kalentivan <kalentivan@yandex.ru>
2025-05-25 16:29:18 +0300

    start.sh

---
+++ backup.sql
@@ -0,0 +1,82 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.13
+-- Dumped by pg_dump version 16.4
+
+-- Started on 2025-05-25 16:28:41
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 4 (class 2615 OID 2200)
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 209 (class 1259 OID 338543)
+-- Name: alembic_version; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.alembic_version (
+    version_num character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.alembic_version OWNER TO postgres;
+
+--
+-- TOC entry 3304 (class 0 OID 338543)
+-- Dependencies: 209
+-- Data for Name: alembic_version; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.alembic_version (version_num) FROM stdin;
+1e457afdd562
+\.
+
+
+--
+-- TOC entry 3164 (class 2606 OID 338547)
+-- Name: alembic_version alembic_version_pkc; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.alembic_version
+    ADD CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num);
+
+
+--
+-- TOC entry 3310 (class 0 OID 0)
+-- Dependencies: 4
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2025-05-25 16:28:42
+
+--
+-- PostgreSQL database dump complete
+--
+
--- start.sh
+++ start.sh
@@ -4,18 +4,5 @@
 # Активация виртуального окружения
 source ./venv/bin/activate
 
-# Ждем, пока Postgres не станет доступен
-until pg_isready -h $DB_HOST -p $TG_DB_PORT -U $POSTGRES_USER; do
-  echo "Waiting for postgres..."
-  sleep 2
-done
-
-# Создаем базу, если ее нет (предполагается, что есть пользователь с нужными правами)
-psql -h $DB_HOST -p $TG_DB_PORT -U $POSTGRES_USER -tc "SELECT 1 FROM pg_database WHERE datname = '${POSTGRES_DB}'" | grep -q 1 || \
-psql -h $DB_HOST -p $TG_DB_PORT -U $POSTGRES_USER -c "CREATE DATABASE ${POSTGRES_DB}"
-
-# Применяем миграции alembic
-alembic upgrade head
-
 # Запуск Uvicorn (не в фоне, оставляем последним)
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
