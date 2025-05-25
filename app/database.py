e87a6ee3e9eb7054ba3128aab3aeccd032bfd1c6
kalentivan <kalentivan@yandex.ru>
2025-05-25 14:52:53 +0300

    start.sh

--- app/database.py
+++ app/database.py
@@ -6,7 +6,12 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{os.getenv('TG_DB_USER')}:{os.getenv('TG_DB_PASSWORD')}"
+    f"@{os.getenv('TG_DB_HOST')}:{os.getenv('TG_DB_PORT')}/"
+    f"{os.getenv('TG_DB_NAME')}"
+)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
