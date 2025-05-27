8479b8215b38e84f213d0a8042d486162059c24b
kalentivan <kalentivan@yandex.ru>
2025-05-27 12:43:47 +0300

    тестирование

--- backup.sql
+++ backup.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 14.13
 -- Dumped by pg_dump version 16.4
 
--- Started on 2025-05-26 00:35:44
+-- Started on 2025-05-27 12:43:41
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -33,7 +33,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 209 (class 1259 OID 349925)
+-- TOC entry 209 (class 1259 OID 420866)
 -- Name: alembic_version; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -45,17 +45,169 @@ CREATE TABLE public.alembic_version (
 ALTER TABLE public.alembic_version OWNER TO postgres;
 
 --
--- TOC entry 3304 (class 0 OID 349925)
+-- TOC entry 211 (class 1259 OID 420931)
+-- Name: chats; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.chats (
+    name character varying,
+    is_group boolean,
+    id uuid NOT NULL
+);
+
+
+ALTER TABLE public.chats OWNER TO postgres;
+
+--
+-- TOC entry 213 (class 1259 OID 420956)
+-- Name: group_members; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.group_members (
+    user_id uuid NOT NULL,
+    chat_id uuid NOT NULL,
+    is_admin boolean
+);
+
+
+ALTER TABLE public.group_members OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 420989)
+-- Name: message_reads; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.message_reads (
+    message_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    read_at timestamp with time zone
+);
+
+
+ALTER TABLE public.message_reads OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 420971)
+-- Name: messages; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.messages (
+    chat_id uuid,
+    sender_id uuid,
+    text text,
+    "timestamp" timestamp with time zone,
+    is_read boolean,
+    id uuid NOT NULL
+);
+
+
+ALTER TABLE public.messages OWNER TO postgres;
+
+--
+-- TOC entry 212 (class 1259 OID 420944)
+-- Name: tokens; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.tokens (
+    jti uuid NOT NULL,
+    user_id uuid NOT NULL,
+    device_id character varying NOT NULL,
+    revoked boolean,
+    expired_time timestamp with time zone
+);
+
+
+ALTER TABLE public.tokens OWNER TO postgres;
+
+--
+-- TOC entry 210 (class 1259 OID 420921)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    username character varying,
+    email character varying,
+    is_admin boolean,
+    password character varying,
+    id uuid NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 3350 (class 0 OID 420866)
 -- Dependencies: 209
 -- Data for Name: alembic_version; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.alembic_version (version_num) FROM stdin;
+370ed91d0ff5
 \.
 
 
 --
--- TOC entry 3164 (class 2606 OID 349929)
+-- TOC entry 3352 (class 0 OID 420931)
+-- Dependencies: 211
+-- Data for Name: chats; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.chats (name, is_group, id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3354 (class 0 OID 420956)
+-- Dependencies: 213
+-- Data for Name: group_members; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.group_members (user_id, chat_id, is_admin) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3356 (class 0 OID 420989)
+-- Dependencies: 215
+-- Data for Name: message_reads; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.message_reads (message_id, user_id, read_at) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3355 (class 0 OID 420971)
+-- Dependencies: 214
+-- Data for Name: messages; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.messages (chat_id, sender_id, text, "timestamp", is_read, id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3353 (class 0 OID 420944)
+-- Dependencies: 212
+-- Data for Name: tokens; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.tokens (jti, user_id, device_id, revoked, expired_time) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3351 (class 0 OID 420921)
+-- Dependencies: 210
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (username, email, is_admin, password, id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3188 (class 2606 OID 420870)
 -- Name: alembic_version alembic_version_pkc; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -64,7 +216,155 @@ ALTER TABLE ONLY public.alembic_version
 
 
 --
--- TOC entry 3310 (class 0 OID 0)
+-- TOC entry 3195 (class 2606 OID 420937)
+-- Name: chats chats_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.chats
+    ADD CONSTRAINT chats_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3200 (class 2606 OID 420960)
+-- Name: group_members group_members_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.group_members
+    ADD CONSTRAINT group_members_pkey PRIMARY KEY (user_id, chat_id);
+
+
+--
+-- TOC entry 3203 (class 2606 OID 420977)
+-- Name: messages messages_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3198 (class 2606 OID 420950)
+-- Name: tokens tokens_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tokens
+    ADD CONSTRAINT tokens_pkey PRIMARY KEY (jti);
+
+
+--
+-- TOC entry 3193 (class 2606 OID 420927)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3196 (class 1259 OID 420943)
+-- Name: ix_chats_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_chats_id ON public.chats USING btree (id);
+
+
+--
+-- TOC entry 3201 (class 1259 OID 420988)
+-- Name: ix_messages_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_messages_id ON public.messages USING btree (id);
+
+
+--
+-- TOC entry 3189 (class 1259 OID 420928)
+-- Name: ix_users_email; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX ix_users_email ON public.users USING btree (email);
+
+
+--
+-- TOC entry 3190 (class 1259 OID 420929)
+-- Name: ix_users_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_users_id ON public.users USING btree (id);
+
+
+--
+-- TOC entry 3191 (class 1259 OID 420930)
+-- Name: ix_users_username; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX ix_users_username ON public.users USING btree (username);
+
+
+--
+-- TOC entry 3205 (class 2606 OID 420961)
+-- Name: group_members group_members_chat_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.group_members
+    ADD CONSTRAINT group_members_chat_id_fkey FOREIGN KEY (chat_id) REFERENCES public.chats(id);
+
+
+--
+-- TOC entry 3206 (class 2606 OID 420966)
+-- Name: group_members group_members_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.group_members
+    ADD CONSTRAINT group_members_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 3209 (class 2606 OID 420994)
+-- Name: message_reads message_reads_message_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message_reads
+    ADD CONSTRAINT message_reads_message_id_fkey FOREIGN KEY (message_id) REFERENCES public.messages(id);
+
+
+--
+-- TOC entry 3210 (class 2606 OID 420999)
+-- Name: message_reads message_reads_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message_reads
+    ADD CONSTRAINT message_reads_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 3207 (class 2606 OID 420978)
+-- Name: messages messages_chat_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_chat_id_fkey FOREIGN KEY (chat_id) REFERENCES public.chats(id);
+
+
+--
+-- TOC entry 3208 (class 2606 OID 420983)
+-- Name: messages messages_sender_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_sender_id_fkey FOREIGN KEY (sender_id) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 3204 (class 2606 OID 420951)
+-- Name: tokens tokens_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tokens
+    ADD CONSTRAINT tokens_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3362 (class 0 OID 0)
 -- Dependencies: 4
 -- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -73,7 +373,7 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2025-05-26 00:35:45
+-- Completed on 2025-05-27 12:43:42
 
 --
 -- PostgreSQL database dump complete
